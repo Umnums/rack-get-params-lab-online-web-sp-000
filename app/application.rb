a32d4f12d2d763f9de611d790f5efeea5fa3eecc
Joseph Kao <josephtkao@gmail.com>
2020-06-27 17:04:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,13 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
+      if @@items.include(search_term)
+        @@cart << search_term
+        resp.write "Added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
